258b6e4c5e505c3c8086a59038d5e56a91d096da
pknelson01 <pk.elliott11@gmail.com>
2022-08-09 20:06:20 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## DevCamp JS Footer
+
+> This should be used in the following manner
+
+Install with the command;
+
+```
+npm install --save devcamp-js-footer
+```
+
+Add to a JavaScript Project with the following code:
+
+```javascript
+import { footer } from 'devcamp-js-footer';
+
+footer('Parker Nelson')
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var moment = require('moment');
+
+/**
+ * returns a string element with a footer and updating year
+ * @param {string} name
+ * @return {string}
+ */
+
+exports.footer = function (name) {
+    return "CopyRight" + moment().format('YYYY') + " " + name + "All rights reserved";
+};
+
+
